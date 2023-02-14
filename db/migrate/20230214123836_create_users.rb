63bd4cdaa2431bfa2b3dc8e4c85cffdc0f6040fe
pro-soichiro <dr.1987626@gmail.com>
2023-02-14 22:05:08 +0900

    add users controller and user model

---
+++ db/migrate/20230214123836_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.string :email
+      t.date :joined_on
+      t.date :born_on
+      t.string :nickname
+      t.string :special_skill
+      t.string :pastime
+      t.string :motto
+      t.text :motto_description
+      t.text :career
+      t.text :self_introduction
+
+      t.timestamps
+    end
+  end
+end
