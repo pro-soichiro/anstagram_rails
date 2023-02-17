e41459f68bc80d865a76b77f3d5d3c10aea8c1a1
pro-soichiro <dr.1987626@gmail.com>
2023-02-18 08:45:35 +0900

    fix visible settings

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,28 +1,35 @@
 module Visible
   extend ActiveSupport::Concern
 
-  VALID_STATUSES = ['公開', '非公開', 'アーカイブ']
+  VALID_STATUSES = ['公開', '非公開']
 
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
 
   class_methods do
-    def public_count
-      where(status: '公開').count
-    end
-
     def public
       where(status: '公開')
     end
+
+    def private
+      where(status: '非公開')
+    end
   end
 
-
-  def public_count
-    where(status: '公開').count
+  def public
+    where(status: '公開')
   end
 
-  def archived?
-    status == 'アーカイブ'
+  def private
+    where(status: '非公開')
+  end
+
+  def public?
+    status == '公開'
+  end
+
+  def private?
+    status == '非公開'
   end
 end
