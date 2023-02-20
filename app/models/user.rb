afee840ae1519fbf03fe2e5219dc28d1828eb34a
pro-soichiro <dr.1987626@gmail.com>
2023-02-20 09:45:36 +0900

    add devise settings and add letter opener web

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+        #  :omniauthable
+
   has_many :posts, dependent: :destroy
   has_and_belongs_to_many :departments
   belongs_to :prefecture, optional: true
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,17 +42,21 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.logger = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            ENV["GMAIL_ADDRESS"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto:  true
-  }
+  config.action_mailer.logger = ActiveSupport::Logger.new("log/mailer.log")
+
+  # メールの送信設定
+  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true
+  # 実際にGmailからメールを送りたい場合はこちらを利用
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                  587,
+  #   domain:               'gmail.com',
+  #   user_name:            ENV["GMAIL_ADDRESS"],
+  #   password:             ENV["GMAIL_PASSWORD"],
+  #   authentication:       :login,
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
