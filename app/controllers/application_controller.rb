2c1e56dece02668d6c9b7b4e6e4c8e1d326f8da1
pro-soichiro <dr.1987626@gmail.com>
2023-02-23 12:16:18 +0900

    fix redirect when sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  protected
-
-  def after_sign_in_path_for(resource)
-    user_path(resource)
-  end
 end
