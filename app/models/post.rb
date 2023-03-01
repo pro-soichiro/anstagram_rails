dd36affeaba14fe8f0be86b6483af50950c11958
pro-soichiro <dr.1987626@gmail.com>
2023-03-01 21:54:24 +0900

    add active storage validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
-  validates :caption, presence: true
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :likes_users, through: :likes, source: :user
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [300, 300]
   end
+
+  validates :caption, presence: true
+  validates :image, attached: true, content_type: %i(png jpg jpeg)
 end
