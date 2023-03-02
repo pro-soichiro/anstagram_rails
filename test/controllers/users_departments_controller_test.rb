d8e1d9a9ac779acd87cbc4781bdd051e0a9f6ec2
pro-soichiro <dr.1987626@gmail.com>
2023-03-02 21:17:26 +0900

    remove test

--- test/controllers/users_departments_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class UsersDepartmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_departments_new_url
-    assert_response :success
-  end
-end
