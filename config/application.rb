3af6605d0909d56cf9692a7d112a2a6f9bde7275
pro-soichiro <dr.1987626@gmail.com>
2023-03-02 21:16:58 +0900

    add rspec

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,13 @@ module Anstagram
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb, yml}')]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
