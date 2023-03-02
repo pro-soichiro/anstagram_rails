08987da36563620326b600b111664148adb19453
pro-soichiro <dr.1987626@gmail.com>
2023-03-03 08:48:09 +0900

    add prefecture factory

---
+++ spec/factories/prefectures.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :prefecture do
+    sequence(:name) { |n| "都道府県名#{n}" }
+  end
+end
--- spec/models/prefecture_spec.rb
+++ spec/models/prefecture_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Prefecture, type: :model do
   it "名前があれば有効な状態であること" do
-    prefecture = Prefecture.new(name: '都道府県名')
-    expect(prefecture).to be_valid
+    expect(FactoryBot.build(:prefecture)).to be_valid
   end
 
   it "名前がなければ無効な状態であること" do
-    prefecture = Prefecture.new(name: nil)
+    prefecture = FactoryBot.build(:prefecture, name: nil)
     prefecture.valid?
     expect(prefecture.errors[:name]).to include("は必須項目です")
   end
