4a3ba088e699cc26df20aa4aa1c2eff3aa7a414f
pro-soichiro <dr.1987626@gmail.com>
2023-03-03 22:15:13 +0900

    add trait

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,6 +6,15 @@ FactoryBot.define do
     password { 'password1234' }
   end
 
+  trait :confirmed do
+    confirmed_at { Time.now }
+  end
+
+  trait :admin do
+    confirmed_at { Time.now }
+    admin { true }
+  end
+
   trait :with_posts do
     after(:create) { |user| create_list(:post, 3, user: user) }
   end
